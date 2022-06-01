bf44b20ace291f5893b078d838e0d4c25ce69b3e
Lasm Gratel <lasm_gratel@hotmail.com>
2022-06-01 23:35:35 +0800

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# mod-compability-checker
-Simple program that checks any client-side only mod
+# mod-compatibility-checker
+
+Simple Rust program that checks mod compatibility. Only supports 1.12.2 by now.
+
+## Usage
+
+Execute `mod-compatibility-checker <your mods directory>` or just place it in your mods directory and execute it.
+
+Help message:
+```
+USAGE:
+    mod-compatibility-checker [OPTIONS] [PATH]
+
+ARGS:
+    <PATH>
+
+OPTIONS:
+    -d, --dirty      Write a .sha or .strict-sha file in target directory
+    -h, --help       Print help information
+        --strict     Hash JAR file instead of modid:version lines
+    -v, --verbose    List all the client-side mods or mods that accept all remote versions while
+                     parsing
+    -V, --version    Print version information
+```
+
+If two or more mods directory output the same checksum, they are compatible and free to join each other.
+
+## Principle
+
+Checks `META-INF/fml_cache_annotation.json` and get the annotation value, marks all client-only and `acceptableRemoteVersions = "*"` mods.
+
+Hash `modid:version` line or JAR file if strict mode is on.
+
+## TODO
+
+- [ ] Optimize performance further
+- [ ] Support 1.7.10
+- [ ] Support 1.13+
+- [ ] Support Fabric
+
+## Benchmark
+
+| Modpack | Avg time | Avg time (Strict) |
+| -- |  -- | -- |
+| Nomifactory | 263.1 ms | 352.6 ms |
+| Enigmatica 2 Expert Skyblock | 313.8 ms | 463.1 ms |
+| FTB Revelation | 354.3 ms | 536.5 ms |
+| FTB University 1.12 | 367.8 ms | 519.1 ms |
+
+## Credits
+
+[BLAKE3](https://github.com/BLAKE3-team/BLAKE3) for rapid fast hashing.
+
+[hyperfine](https://github.com/sharkdp/hyperfine) for benchmarking.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
+extern crate core;
+
+use std::cmp::Ordering;
 use futures::{FutureExt, stream, StreamExt, TryFutureExt, TryStreamExt};
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 use std::ffi::OsString;
 use std::fs::File;
-use std::io::{BufReader, Cursor, Read};
-use std::path::Path;
-use std::sync::atomic::AtomicU64;
+
+use std::io::{Cursor, ErrorKind, Read};
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
-use futures::executor::block_on;
+use blake3::Hasher;
 use futures::future::Ready;
-use memmap2::MmapMut;
 use rayon::prelude::*;
 use tokio_stream::wrappers::ReadDirStream;
-use zip::result::ZipResult;
 use serde::{Serialize, Deserialize};
-
+use clap::Parser;
+use memmap2::Mmap;
 use mimalloc::MiMalloc;
 use tokio::fs::DirEntry;
 use crate::stream::TryFilter;
@@ -39,11 +41,35 @@ struct ModInfo {
     mcversion: Option<String>,
 }
 
-#[derive(PartialEq, Debug, Serialize)]
+#[derive(Debug, Serialize)]
 struct Mod {
     id: String,
+
+    #[serde(skip_serializing)]
+    file_name: String,
+
     version: Option<String>,
     mod_type: ModType,
+}
+
+impl PartialEq for Mod {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id && self.version == other.version
+    }
+}
+
+impl Eq for Mod {}
+
+impl PartialOrd<Self> for Mod {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.id.partial_cmp(&other.id)
+    }
+}
+
+impl Ord for Mod {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.id.cmp(&other.id)
+    }
 }
 
 #[derive(PartialEq, Debug, Serialize)]
@@ -86,7 +112,7 @@ struct ClassEntry {
     pub interfaces: Option<Vec<String>>,
 }
 
-async fn walk_jar<P: AsRef<Path>>(path: P) -> std::io::Result<TryFilter<ReadDirStream, Ready<bool>, fn(&DirEntry) -> Ready<bool>>> {
+async fn walk_dir<P: AsRef<Path>>(path: P) -> std::io::Result<TryFilter<ReadDirStream, Ready<bool>, fn(&DirEntry) -> Ready<bool>>> {
     let stream = ReadDirStream::new(tokio::fs::read_dir(path)
         .await?);
     Ok(stream
@@ -100,40 +126,92 @@ async fn walk_jar<P: AsRef<Path>>(path: P) -> std::io::Result<TryFilter<ReadDirS
         }))
 }
 
-fn read_archive<P: AsRef<Path>>(path: P) -> Result<(HashMap<String, ClassEntry>, Option<Vec<ModInfo>>), Box<dyn std::error::Error>> {
+fn read_archive<P: AsRef<Path>>(path: P) -> Result<(String, Option<HashMap<String, ClassEntry>>, Option<Vec<ModInfo>>, bool), Box<dyn std::error::Error>> {
     let path = path.as_ref();
+    let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+    if file_name.as_str().to_ascii_lowercase().contains("optifine") {
+        return Ok((file_name, None, None, true)); // FUCK OPTIFINE
+    }
     let file = File::open(path)?;
 
     let mmap = unsafe { memmap2::Mmap::map(&file) }?;
     let cursor = Cursor::new(mmap);
     let mut archive = zip::ZipArchive::new(cursor)?;
 
-    let annotations: HashMap<String, ClassEntry> = match archive.by_name("META-INF/fml_cache_annotation.json") {
+    let annotations: Option<HashMap<String, ClassEntry>> = match archive.by_name("META-INF/fml_cache_annotation.json") {
         Ok(mut f) => {
             let mut str = String::with_capacity(f.size() as usize);
-            f.read_to_string(&mut str);
+            f.read_to_string(&mut str)?;
             simd_json::serde::from_str(&mut str)//serde_json::from_str(&str)
-                .expect(&format!("JSON error while parsing file {:?}", path))
+                .unwrap_or_else(|_| panic!("JSON error while parsing file {:?}", path))
         },
-        Err(e) => {
-            return Err(e.into());
+        Err(_) => {
+            None
         }
     };
 
     let info: Option<Vec<ModInfo>> = match archive.by_name("mcmod.info") {
         Ok(mut f) => {
             let mut str = String::with_capacity(f.size() as usize);
-            f.read_to_string(&mut str);
-            str = str.replace("\n", "");
-            Some(read_mcmod_info(&mut str)
-                .expect(&format!("JSON error while parsing mcmod.info from file {:?}", path)))
+            f.read_to_string(&mut str)?;
+            str = str.replace('\n', "");
+            read_mcmod_info(&mut str).ok()
         },
-        Err(e) => {
-            return Err(e.into());
+        Err(_) => {
+            None
         }
     };
 
-    Ok((annotations, info))
+    Ok((file_name, annotations, info, false))
+}
+
+fn parse_mods(file_name: String, entries: Option<HashMap<String, ClassEntry>>, mod_infos: Option<Vec<ModInfo>>, is_optifine: bool) -> Vec<Mod> {
+    if is_optifine {
+        return vec![Mod { id: String::from("OptiFine"), file_name, version: None, mod_type: ModType::ClientOnly }]
+    }
+
+    let mut mods = vec![];
+    if let Some(entries) = entries {
+        if let Some(entries) = parse_entries(entries) {
+            let mut mod_map = entries.into_iter().map(|(mod_type, id, version)| {
+                (id.clone(), Mod {
+                    id,
+                    file_name: file_name.clone(),
+                    version,
+                    mod_type
+                })
+            }).collect::<HashMap<String, Mod>>();
+
+            // If there is mcmod.info file, match mod id and append versions
+            if let Some(infos) = mod_infos {
+                // Map consists modid and infos
+                infos.into_iter().for_each(|info| {
+                    if let Some(x) = mod_map.get_mut(&info.modid) {
+                        x.version = Some(info.version);
+                    }
+                });
+            }
+            mod_map.into_iter().for_each(|(_, x)| mods.push(x));
+        }
+    }
+
+    mods
+}
+
+// TODO 1.7.10
+fn parse_entries(entries: HashMap<String, ClassEntry>) -> Option<Vec<(ModType, String, Option<String>)>> {
+    Some(entries
+        .into_par_iter()
+        .filter_map(|(_, entry)| {
+            entry.annotations
+        })
+        .flatten()
+        .filter(|x| x.name == "Lnet/minecraftforge/fml/common/Mod;")
+        .filter_map(|x| {
+            decl_mod_type(&x)
+        })
+        .collect()
+    )
 }
 
 /// Read mcmod.info string
@@ -145,96 +223,101 @@ fn read_mcmod_info(s: &mut str) -> Result<Vec<ModInfo>, simd_json::Error> {
     }
 }
 
+#[derive(Parser, Debug)]
+#[clap(author, version)]
+struct Args {
+    /// Mods directory
+    #[clap(parse(from_os_str))]
+    path: Option<std::path::PathBuf>,
+
+    /// List all the client-side mods or mods that accept all remote versions while parsing.
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Hash JAR file instead of modid:version lines
+    #[clap(long)]
+    strict: bool,
+
+    /// Write a .sha or .strict-sha file in current directory.
+    #[clap(short, long)]
+    dirty: bool,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = std::env::args_os().skip(1).next().unwrap_or(OsString::from("."));
-    let start = Instant::now();
-    let stream = ReadDirStream::new(tokio::fs::read_dir(path)
-        .await?);
-    let stream = stream
-        .try_filter(|file| {
-            futures::future::ready(
-                file.file_name()
-                    .to_str()
-                    .map(|x| x.ends_with(".jar"))
-                    .unwrap_or(false),
-            )
-        })
+    let args: Args = Args::parse();
+    let path = args.path.unwrap_or_default();
+
+    let stream = walk_dir(&path).await?
         .try_filter_map(|file| async move {
-            //let start = Instant::now();
-            let path = &file.path();
-            let file_name = file.file_name().to_string_lossy().to_string();
-            if file_name.as_str().to_ascii_lowercase().contains("optifine") {
-                return Ok(Some((file_name, None, None, true))); // FUCK OPTIFINE
+            match read_archive(file.path()) {
+                Ok(x) => { Ok(Some(x)) }
+                Err(e) => { Err(std::io::Error::new(ErrorKind::Other, e.to_string())) }
             }
-            read_archive(path)
         })
         .try_filter_map(|(name, map, infos, is_optifine)| async move {
-            if is_optifine {
-                return Ok(Some(futures::stream::iter(vec![Ok(Mod { id: String::from("OptiFine"), version: None, mod_type: ModType::ClientOnly })].into_iter())));
-            }
-            let infos = infos.unwrap_or(vec![]).into_iter().map(|x| (x.modid.to_string(), x)).collect::<HashMap<String, ModInfo>>();
-            let map = map.unwrap();
-            Ok(Some(futures::stream::iter(map.into_par_iter().flat_map(|(name, entry)| {
-                match entry.annotations {
-                    None => {
-                        vec![].into_par_iter()
-                    }
-                    Some(x) => {
-                        x.into_par_iter()
-                    }
-                }
-            }).filter_map(|annotation| {
-                if annotation.name == "Lnet/minecraftforge/fml/common/Mod;" {
-                    Some(decl_mod_type(&annotation))
-                } else {
-                    None
-                }
-            }).filter_map(|o| {
-                if let Some((t, modid)) = o {
-                    if let Some(info) = infos.get(&modid) {
-                        return Some(Ok(Mod {
-                            id: modid,
-                            mod_type: t,
-                            version: Some(info.version.to_string())
-                        }));
-                    }
-                }
-                None
-            }).collect::<Vec<std::io::Result<Mod>>>().into_iter())))
-        }).try_flatten();
+            Ok(Some(futures::stream::iter(parse_mods(name, map, infos, is_optifine).into_iter().map(Ok).collect::<Vec<std::io::Result<Mod>>>())))
+        })
+        .try_flatten();
 
-    let mut i = Arc::new(Mutex::new(0u64));
+    let (tx, rx) = crossbeam_channel::unbounded();
 
     stream
         .try_for_each_concurrent(32, |mod_object| {
-            let mut i = i.clone();
+            let tx = tx.clone();
             async move {
-                *(i.lock().unwrap()) += 1;
-                println!("{}", mod_object.id);
+                if mod_object.mod_type != ModType::Normal {
+                    if args.verbose {
+                        println!("{}", mod_object.file_name);
+                    }
+                } else {
+                    tx.send(mod_object).map_err(|e| std::io::Error::new(ErrorKind::Other, e))?;
+                }
                 Ok(())
             }
         })
         .await?;
 
-    println!(
-        "Found {} client-side mods in {}ms",
-        i.lock().unwrap(),
-        start.elapsed().as_millis()
-    );
+    let mut hash = Hasher::new();
+    let x: std::io::Result<()> = rx.try_iter().collect::<BTreeSet<Mod>>().into_iter().try_for_each(|x| {
+        if args.strict {
+            let file = File::open(path.join(&x.file_name))?;
+            let mmap = unsafe { Mmap::map(&file) }?;
+            hash.update_rayon(&mmap);
+        } else {
+            hash.update(format!("{}:{}", &x.id, x.version.unwrap_or_default()).as_bytes());
+        }
+        Ok(())
+    });
+    let _ = x?;
+    let hash = hash.finalize();
+    println!("{}", hash.to_hex());
+
+    if args.dirty {
+        if args.strict {
+            tokio::fs::write(path.join(".strict-sha"), hash.to_hex().to_string()).await?
+        } else {
+            tokio::fs::write(path.join(".sha"), hash.to_hex().to_string()).await?
+        }
+    }
+
     Ok(())
 }
 
-fn decl_mod_type(annotation: &Annotation) -> Option<(ModType, String)> {
-    if let Some(values) = &annotation.values {
-        let client_only = values.get("clientSideOnly").map(|x| x.value.as_ref().map(|x| x == "true")).flatten().unwrap_or(false);
+fn decl_mod_type(annotation: &Annotation) -> Option<(ModType, String, Option<String>)> {
 
-        let accept_all_remote = values.get("acceptableRemoteVersions").map(|x| x.value.as_ref().map(|x| x == "*")).flatten().unwrap_or(false);
+    if let Some(values) = &annotation.values {
+        let client_only = values.get("clientSideOnly").and_then(|x| x.value.as_ref().map(|x| x == "true")).unwrap_or(false);
+
+        let accept_all_remote = values.get("acceptableRemoteVersions").and_then(|x| x.value.as_ref().map(|x| x == "*")).unwrap_or(false);
+        let version = values.get("version").and_then(|x| x.value.clone());
         let modid = values["modid"].value.as_ref()?.to_string();
-        if client_only {
-            return Some((ModType::ClientOnly, modid));
+        return if client_only {
+            Some((ModType::ClientOnly, modid, version))
         } else if accept_all_remote {
-            return Some((ModType::AcceptAllRemote, modid));
+            Some((ModType::AcceptAllRemote, modid, version))
+        } else {
+            Some((ModType::Normal, modid, version))
         }
     }
     None
